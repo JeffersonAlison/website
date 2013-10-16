0afeb294ea7daa77c34d7ff70a6f92b7c94c843b
dev2 <egenial@Mac-mini-de-Carlos-Eduardo-G-Franco.local>
2013-10-16 17:22:16 -0400

    nd alterado

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+    unless Rails.application.config.consider_all_requests_local
+		rescue_from Exception, :with => :render_500
+		rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+		rescue_from ActionController::RoutingError, :with => :render_404
+	end
+
+	def render_500
+		render :template => "errors/500.html.erb", :layout => "message", :status => "500"
+	end
+
+	def render_404
+		render :template => "errors/404.html.erb", :layout => "message",  :status => "404"
+	end  
 	 def resource_name
 	    :user
 	  end
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,5 +2,7 @@ class SiteController < ApplicationController
 	layout "application"
   def index
   	@postnotices = Postnotice.all
+  	@banners = Banner.all.take(4)
+
   end
 end
